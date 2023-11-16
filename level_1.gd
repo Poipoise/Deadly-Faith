d425a0913f3078081ce814253e3ea561ad513e47
Anthony Melgar <amelgar@Cart2-21.local>
2023-11-16 12:20:55 -0800

    worked on title screen

--- level_1.gd
+++ level_1.gd
@@ -2,3 +2,8 @@ extends Node2D
 
 func _process(delta):
 	$CanvasLayer/StaminaBar.value = $Player.stamina
+
+
+
+func _on_start_screen_start_game():
+	pass # Replace with function body.
---
+++ start_screen.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+signal start_game
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
