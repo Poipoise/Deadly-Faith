858ec16092791e57e3c7db7eb8bfd76fdac0b4f9
Andrew <apalchevs0001@mymail.lausd.net>
2023-12-08 12:21:41 -0800

    necromancer gaming

--- Executioner.gd
+++ Executioner.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var speed = 150
-enum states {IDLE, CHASE, ATTACK, DEAD, HURT,SUMMONING}
+enum states {IDLE, TELEPORT, ATTACK, DEAD, HURT,}
 var state = states.IDLE
 var player
 var attacking = false
@@ -31,8 +31,8 @@ func choose_action():
 		states.IDLE:
 			$AnimationPlayer.play("idle")
 			velocity = Vector2.ZERO
-		states.CHASE:
-			$AnimationPlayer.play("run")
+		states.TELEPORT:
+			$AnimationPlayer.play("teleport")
 			velocity = position.direction_to(player.position) * speed
 			if velocity.x != 0:
 				transform.x.x = sign(velocity.x)
@@ -54,21 +54,6 @@ func hurt(amount, dir):
 	if health <= 0:
 		state = states.DEAD
 
-func _on_detect_body_entered(body):
-	player = body
-	state = states.CHASE
-
-func _on_detect_body_exited(body):
-	state = states.IDLE
-
-func _on_attack_body_entered(body):
-	state = states.ATTACK
-
-func _on_attack_body_exited(body):
-	if attacking:
-		await $AnimationPlayer.animation_finished
-	state = states.CHASE
-
 
 
 func _on_ghoul_hurt_box_body_entered(body):
@@ -82,3 +67,22 @@ func respawn():
 	state = states.IDLE
 	if summoned:
 		queue_free()
+
+
+func _on_chase_body_entered(body):
+	player = body
+	state = states.TELEPORT
+
+
+func _on_chase_body_exited(body):
+	state = states.IDLE
+
+
+func _on_attack_box_body_entered(body):
+	state = states.ATTACK
+	
+func _on_attack_box_body_exited(body):
+	if attacking:
+		await $AnimationPlayer.animation_finished
+	state = states.TELEPORT
+
